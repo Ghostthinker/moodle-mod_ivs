ed58ae599981eb1b1a0debd44f234a171997c29b
Sergej Naumenko <office@naumenko-multimedia.de>
2021-03-15 14:18:20 +0100

    fix 4:3 video thumbnail preview overlapping

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_ivs';
-$plugin->release = 'v1.5.412';
-$plugin->version = 2021011300;
+$plugin->release = 'v1.5.419';
+$plugin->version = 2021031500;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_BETA;
 $plugin->cron = 0;
