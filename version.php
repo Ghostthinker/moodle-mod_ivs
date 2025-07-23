e6c54462da21b456d1fc0a8455cba3d14502b1ab
n-multimedia <office@naumenko-multimedia.de>
2025-07-23 11:06:40 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_ivs';
 
-$plugin->release = 'v1.30.164';
+$plugin->release = 'v1.31.164';
 $plugin->version = 2025072300;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
